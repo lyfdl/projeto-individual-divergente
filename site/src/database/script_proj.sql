0446e7527d3e2cfe6b22cd5409cbd4c7004d81b7
lyfdl <livia.lanes@sptech.school>
2023-05-30 12:53:20 -0300

    atualização com footer

--- site/src/database/script_proj.sql
+++ site/src/database/script_proj.sql
@@ -9,3 +9,46 @@ CREATE TABLE usuario (
 );
 
 
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+select * from aviso;
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+select * from medida;
+select count(*), second(momento) from medida group by second(momento);
+select * from mural;
+create table mural (
+idMural int primary key auto_increment,
+autor varchar(45),
+mensagem varchar(250),
+fkUsuario int, foreign key (fkUsuario) references usuario(id)
+);
+select count(*) from aviso where fk_usuario = 3;
+
+select * from aviso;
+select * from usuario;
+alter table aviso add column dtPostagem datetime default current_timestamp; 
+
+
